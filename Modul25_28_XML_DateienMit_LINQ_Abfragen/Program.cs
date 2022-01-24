cd116986f657ebd014114683cb9836248175f4f0
AchimRegez <51678296+AchimRegez@users.noreply.github.com>
2022-01-24 17:53:13 +0100

    Modul 25

---
+++ Modul25_28_XML_DateienMit_LINQ_Abfragen/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace Modul25_28_XML_DateienMit_LINQ_Abfragen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument customers = XDocument.Load(@"C:\Users\Thanatos\source\repos\ProgrammierenStarten\Modul25_28_XML_DateienMit_LINQ_Abfragen\customers.xml");
+
+            //foreach(var element in customers.Descendants("Kunde"))
+            //{
+            //    Console.WriteLine(element.Name);
+            //}
+
+            var firstNameQuery = from customer in customers.Descendants("Kunde")
+                                 select customer.Element("Vorname").Value;
+
+            foreach(string name in firstNameQuery)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            var customersInLondon = from customer in customers.Descendants("Kunde")
+                                    where customer.Element("Adresse").Attribute("Ort").Value == "London"
+                                    select customer;
+
+            Console.WriteLine();
+            Console.WriteLine("Alle Kunden aus London: ");
+            foreach (var customer in customersInLondon)
+            {
+                Console.WriteLine(customer.Element("Vorname").Value + " " + customer.Element("Nachname").Value + " " + customer.Element("Adresse").Attribute("Strasse") + " " + customer.Element("Adresse").Attribute("Ort"));
+            }
+        }
+    }
+}
---
+++ Modul25_29_XML_DateienBearbeitenMit_LINQ_to_XML/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+
+namespace Modul25_29_XML_DateienBearbeitenMit_LINQ_to_XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument customers = XDocument.Load(@"C:\Users\Thanatos\source\repos\ProgrammierenStarten\Modul25_28_XML_DateienMit_LINQ_Abfragen\customers.xml");
+
+            var customersInLondon = from customer in customers.Descendants("Kunde")
+                                    where customer.Element("Adresse").Attribute("Ort").Value == "London"
+                                    select customer;
+
+            foreach(var customer in customersInLondon)
+            {
+                customer.Element("Vorname").Value = "NEUER NAME";
+            }
+
+            foreach(var customer in customersInLondon)
+            {
+                customer.SetElementValue("Vorname", "ZWEITER NEUER NAME");
+            }
+
+            foreach (var customer in customersInLondon)
+            {
+                customer.SetElementValue("NeuesElement", "Hallo World"); //Methode SetElementValue erstellt den Eintrag wenn nicht vorhanden !ACHTUNG!: string? darf kein Leerzeichen enthalten -> "NeuesElement"
+            }
+
+            //foreach (var customer in customersInLondon)
+            //{
+            //    customer.SetElementValue("NeuesElement", null); //Methode SetElementValue löscht das Element
+            //}
+
+            foreach (var customer in customersInLondon)
+            {
+                customer.SetAttributeValue("test", 10);
+            }
+
+            //foreach (var customer in customersInLondon)
+            //{
+            //    customer.SetAttributeValue("test", null);
+            //}
+
+            foreach(var customer in customersInLondon)
+            {
+                customer.RemoveAll();
+            }
+
+
+            customers.Save(@"C:\Users\Thanatos\source\repos\ProgrammierenStarten\Modul25_29_XML_DateienBearbeitenMit_LINQ_to_XML\customers.xml");
+
+        }
+    }
+}
