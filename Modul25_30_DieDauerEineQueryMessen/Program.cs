54aba02ecf804c50e459d30736bb066bff90191e
AchimRegez <51678296+AchimRegez@users.noreply.github.com>
2022-01-27 17:28:13 +0100

    Fertig Modul 25

---
+++ Modul25_30_DieDauerEineQueryMessen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Modul25_30_DieDauerEineQueryMessen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erstelle Stoppuhr und das Array
+            Stopwatch sw = new Stopwatch();
+            var numbers = Enumerable.Range(1, 100000000);
+
+            //Zwei Beispiel-Queries
+            var query = from num in numbers
+                        where num % 12345678 == 0
+                        select num;
+
+            var query2 = from num in numbers.AsParallel()
+                         where num % 12345678 == 0
+                         select num;
+
+            Console.WriteLine("Loading...");
+
+            //Query ohne Parallelisierung
+            sw.Start();
+
+            foreach (int num in query)
+                Console.WriteLine(num);
+
+            sw.Stop();
+            Console.WriteLine("Without PLINQ: " + sw.ElapsedMilliseconds);
+
+            sw.Reset();
+
+
+            //Query mit Parallelisierung
+            sw.Start();
+            foreach (int num in query2)
+                Console.WriteLine(num);
+
+            sw.Stop();
+            Console.WriteLine("With PLINQ: " + sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modul25_31_EinführungInParallel_LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Modul25_31_EinführungInParallel_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nubmers = Enumerable.Range(1, 100).ToArray();
+
+            var allEvenNumbers = from num in nubmers
+                                 where num % 2 == 0
+                                 select num;
+
+            foreach (int number in allEvenNumbers)
+                Console.WriteLine(number);
+
+
+
+            int[] nubmersParallel = Enumerable.Range(1, 100).ToArray();
+
+            var allEvenNumbersParallel = from num in nubmers.AsParallel().AsOrdered()
+                                 where num % 2 == 0
+                                 select num;
+
+            foreach (int number in allEvenNumbersParallel)
+                Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
